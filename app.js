2c1c35e1270a2abee2c8b64806580a9ed7e2d0cf
Arslan <arslanbutt7545@gmail.com>
2021-04-24 13:19:57 +0500

    Add chain route

--- app.js
+++ app.js
@@ -25,7 +25,8 @@ const articleSchema = {
 const Article = mongoose.model("Article",articleSchema);
 
 
-app.get('/articles',function(req,res){
+app.route('/articles')
+.get(function(req,res){
   Article.find({},function(err,foundArticles){
     if(!err){
       //console.log(foundArticles);
@@ -34,10 +35,8 @@ app.get('/articles',function(req,res){
       res.send(err);
     }
   });
-});
-
-
-app.post('/articles',function(req,res){
+})
+.post(function(req,res){
   const title = req.body.title;
   const content = req.body.content;
 
@@ -45,7 +44,6 @@ app.post('/articles',function(req,res){
     title : title,
     content : content
   });
-
   article.save(function(err){
     if(!err){
       res.send("Success");
@@ -53,10 +51,8 @@ app.post('/articles',function(req,res){
       res.send(err);
     }
   });
-
-});
-
-app.delete('/articles',function(req,res){
+})
+.delete(function(req,res){
   Article.deleteMany(function(err){
     if(!err){
       res.send("Success");
